197d6029975d6ac85219fcc2ff284bf244c9b4db
ycedenog <yonmaced@espol.edu.ec>
2021-08-12 10:46:40 -0500

    Cambios en el metodo calcularNota

--- TalllerRefactoring/src/modelos/Estudiante.java
+++ TalllerRefactoring/src/modelos/Estudiante.java
@@ -78,31 +78,18 @@ public class Estudiante{
         this.telefono = telefono;
     }
     
+
     //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    public double CalcularNotaInicial(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
-        double notaInicial=0;
+    public double CalcularNota(Paralelo p, double  nexamen, double ndeberes, double nlecciones, double ntalleres){
+        double notaGeneral=0;
         for(Paralelo par:paralelos){
             if(p.equals(par)){
                 double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
                 double notaPractico=(ntalleres)*0.20;
-                notaInicial=notaTeorico+notaPractico;
+                notaGeneral=notaTeorico+notaPractico;
             }
         }
-        return notaInicial;
-    }
-    
-    //Calcula y devuelve la nota final contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    
-    public double CalcularNotaFinal(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
-        double notaFinal=0;
-        for(Paralelo par:paralelos){
-            if(p.equals(par)){
-                double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
-                double notaPractico=(ntalleres)*0.20;
-                notaFinal=notaTeorico+notaPractico;
-            }
-        }
-        return notaFinal;
+        return notaGeneral;
     }
     
     //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. Esta nota es solo el promedio de las dos calificaciones anteriores.
