26dd42e3d894b0243c1218fc33089bd5993bb263
Josealb97 <jame1197@hotmail.com>
2021-08-12 10:59:58 -0500

    Inapropiate intimacy

--- TalllerRefactoring/src/modelos/Profesor.java
+++ TalllerRefactoring/src/modelos/Profesor.java
@@ -26,10 +26,6 @@ public class Profesor {
         paralelos.add(p);
     }
     
-    public double calcularSueldo(Profesor prof){
-        double sueldo=0;
-        sueldo= prof.info.añosdeTrabajo*600 + prof.info.BonoFijo;
-        return sueldo;
-    }  
+   
     
 }
